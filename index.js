a78db7aebea2b80c2e90d7daa716ff0e111ba1a8
iSwat <swatstj@gmail.com>
2026-01-07 14:45:38 -0600

    Log PORT variable from environment
    Add logging for the PORT variable from environment

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
+console.log(PORT from env:",process.env.PORT);
 
   
  
