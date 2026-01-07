df2d075698a72c67200281bc6821ff71c5c1f834
iSwat <swatstj@gmail.com>
2026-01-07 07:07:40 -0600

    Add uploads route and update home links

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const express = require("express");
 const app = express();
 
-// Allow form submissions
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 const projectsRouter = require("./routes/projects");
+const uploadsRouter = require("./routes/uploads");
 
 // Home
 app.get("/", (req, res) => {
   res.send(`
     <h1>🎬 Studio Dashboard</h1>
-    <p><a href="/projects">Go to Projects</a></p>
+    <ul>
+      <li><a href="/projects">Projects</a></li>
+      <li><a href="/uploads">Uploads</a></li>
+    </ul>
   `);
 });
 
-// Projects routes
 app.use("/projects", projectsRouter);
+app.use("/uploads", uploadsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
 
   
  
