5add18b7871bc0970217c2844d156604de49e899
iSwat <swatstj@gmail.com>
2026-01-07 17:07:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
-
+console.log("KEY PRESENT:",process.env.GOOGLE_API_KEY?"YES":"NO");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
